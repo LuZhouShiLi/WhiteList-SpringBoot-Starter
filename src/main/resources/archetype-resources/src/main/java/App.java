d5cbd36f4ec2aaa128ce6141d764621164998099
xxf <luzhoushili@outlook.com>
2024-02-12 15:39:19 +0800

    first commit

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.ustc.whitelist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
